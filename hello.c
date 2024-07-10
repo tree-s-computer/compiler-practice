d0e68bc76ba977fc254c3142dfd188149a99c48c
tree <hhyjs2r@gmail.com>
2024-07-11 00:29:18 +0900

    test: 예시코드, malloc 으로 동적 메모리 할당

--- hello.c
+++ hello.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int* fun() {
+    int* a = (int*)malloc(sizeof(int)); // 동적 메모리 할당
+    *a = 2;
+    return a;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int* p = fun();
+    *p = 20;
+    printf("%d\n", *p); // p가 가리키는 값을 출력
+    free(p); // 동적 메모리 해제
     return 0;
 }
+
+
+// int* func(){
+//     int a =2;
+//     return &a;
+// }//  이 로컬 변수는 함수가 종료되면 메모리가 해제되므로, 이를 참조하면 예기치 않은 동작이나 프로그램 충돌을 유발할 수 있음.
+
+
+// void main(){
+//     int* p = func();
+//     *p = 20;
+// }
